efd0f7ee094a51a03b5da8ec1e994ae594bbbf0c
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-02-28 10:26:56 +0300

    Added functionality for loggining in and capturing user details.

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,16 +1,14 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {GlobalService} from './global.service';
-import {AuthService} from './auth.service';
-import {map} from 'rxjs/operators';
-import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { GlobalService } from './global.service';
+import { AuthService } from './auth.service';
+import { map, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
-@Injectable(
-  {
-    providedIn: 'root',
-  }
-)
+@Injectable({
+  providedIn: 'root',
+})
 export class HttpService {
 
   constructor(
@@ -18,42 +16,74 @@ export class HttpService {
     private globalService: GlobalService,
     private authService: AuthService,
     private router: Router
-  ) {
-  }
-
+  ) {}
 
   public mobileBankingLogin(endpoint: string, model: any): Observable<any> {
-    return this.http.post(this.globalService.mobileBankingHost + endpoint,
-      model,
-      this.generateLoginHeaders()
-    )
+    return this.http
+      .post(
+        this.globalService.mobileBankingHost + endpoint,
+        model,
+        this.generateLoginHeaders()
+      )
       .pipe(
-        map((result: any) => {        
-          localStorage.setItem('isLoggedin', 'true')
-          localStorage.setItem('access_token', result['access_token'])
-          this.router.navigate(['/dashboard'])
+        map((result: any) => {
+          localStorage.setItem('isLoggedin', 'true');
+          localStorage.setItem('access_token', result['access_token']);
+          this.router.navigate(['/dashboard']);
+
+          this.mobileBankingGetUserDetails();
           return result;
-        }));
+        })
+      );
+  }
+
+  public mobileBankingGetUserDetails(): Observable<any> {
+    return this.http
+      .post(
+        this.globalService.mobileBankingHost +
+          'api/v1/corporate/admin/corporate/details',
+        {},
+        this.getHeaders()
+      )
+      .pipe(
+        map((result: any) => {
+          localStorage.setItem(
+            'userData',
+            JSON.stringify(result['data']['corporate'])
+          );
+          return result['data']['corporate'];
+        })
+      );
   }
 
   public mobileBankingPost(endpoint: string, model: any): any {
-    return this.http.post(this.globalService.mobileBankingHost + endpoint,
-      model,
-      this.getHeaders()
-    ).pipe(map(response => {
-      response = response;
-      return response;
-    }));
+    return this.http
+      .post(
+        this.globalService.mobileBankingHost + endpoint,
+        model,
+        this.getHeaders()
+      )
+      .pipe(
+        map((response) => {
+          response = response;
+          return response;
+        })
+      );
   }
 
   // For Pagination
   public mobileBankingPaginationPost(endpoint: string, model: any): any {
     const updatedModel = {
-      page: (model.page - 1),
-      size: model.size
+      page: model.page - 1,
+      size: model.size,
     };
 
-    return this.http.post(this.globalService.mobileBankingHost + endpoint, updatedModel, this.getHeaders())
+    return this.http
+      .post(
+        this.globalService.mobileBankingHost + endpoint,
+        updatedModel,
+        this.getHeaders()
+      )
       .pipe(
         map((response) => {
           response = response;
@@ -64,21 +94,26 @@ export class HttpService {
 
   // endpoint for submitting form Data
   public mobileBankingFormRequestPost(endpoint: string, model: any): any {
-    return this.http.post(this.globalService.mobileBankingHost + endpoint,
-      model,
-      this.getFormHeaders()
-    ).pipe(map(response => {
-      response = response;
-      return response;
-    }));
+    return this.http
+      .post(
+        this.globalService.mobileBankingHost + endpoint,
+        model,
+        this.getFormHeaders()
+      )
+      .pipe(
+        map((response) => {
+          response = response;
+          return response;
+        })
+      );
   }
 
   private getHeaders(): any {
     return {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + this.globalService.getToken()
-      })
+        Authorization: 'Bearer ' + this.globalService.getToken(),
+      }),
     };
   }
 
@@ -86,9 +121,8 @@ export class HttpService {
     return {
       headers: new HttpHeaders({
         // 'Content-Type': 'multipart/form-data',
-        Authorization: 'Bearer ' + this.globalService.getToken()
-
-      })
+        Authorization: 'Bearer ' + this.globalService.getToken(),
+      }),
     };
   }
 
@@ -96,8 +130,9 @@ export class HttpService {
     return {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + btoa('CORPORATE_ADMIN' + ':' + 'YP@kduzzbm#YfkJX')
-      })
+        Authorization:
+          'Basic ' + btoa('CORPORATE_ADMIN' + ':' + 'YP@kduzzbm#YfkJX'),
+      }),
     };
   }
 }
