0c8a64aad2cda0d7c76d8dd77b2aa0a4bd45aa7a
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-03-13 12:33:56 +0300

    Changed profile from SUPER_ADMIN to CORPORATE_ADMIN

--- src/app/views/layout/sidebar/menu.ts
+++ src/app/views/layout/sidebar/menu.ts
@@ -2,96 +2,96 @@ import { MenuItem } from './menu.model';
 
 export const MENU: MenuItem[] = [
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Main',
     isTitle: true
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Dashboard',
     icon: 'home',
     link: '/dashboard'
   },
 
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Products',
     isTitle: true
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Products',
     icon: 'home',
     link: '/mobile-banking/products/list-customers'
   },
 
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Customers Module',
     isTitle: true
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Customers',
     icon: 'file-text',
     link: '/mobile-banking/customers/list-customers'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Failed Registrations',
     icon: 'home',
     link: '/mobile-banking/customers/list-failed-registrations'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Send Bulk SMS',
     icon: 'home',
     link: '/mobile-banking/customers/send-bulk-sms'
   },
 
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Setup',
     isTitle: true,
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Regions',
     icon: 'file-text',
     link: '/mobile-banking/setups/list-Regions'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Branches',
     icon: 'file-text',
     link: '/mobile-banking/setups/list-branches'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'ATMs',
     icon: 'file-text',
     link: '/mobile-banking/setups/list-atms'
   },
 
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'User Management',
     isTitle: true
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Users',
     icon: 'file-text',
     link: '/mobile-banking/Users/list-users'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Roles',
     icon: 'home',
     link: '/mobile-banking/rbac/all-roles'
   },
   {
-    profiles: ['SUPER_ADMIN'],
+    profiles: ['CORPORATE_ADMIN'],
     label: 'Profiles',
     icon: 'home',
     link: '/mobile-banking/rbac/all-profiles'
