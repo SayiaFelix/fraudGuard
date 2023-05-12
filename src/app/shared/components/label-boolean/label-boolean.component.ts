dbb69602d0c30b0f6beb81a50b47b63353ea3982
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-05-13 00:41:27 +0300

    Updated spinner on rbac tables to be positioned in the center.
    Removed unused console logs.
    
    Updated styling of the app-label-boolean component.

--- src/app/shared/components/label-boolean/label-boolean.component.ts
+++ src/app/shared/components/label-boolean/label-boolean.component.ts
@@ -16,13 +16,13 @@ export class LabelBooleanComponent implements OnInit {
   ngOnInit(): void {
     if ( this.value === 1 || this.value === '1' || this.value === true || this.value === 'true' || this.value === 'TRUE' || this.value === 'True') {
       this.label = 'Yes';
-      this.labelClass = 'badge badge-success mr-1';
+      this.labelClass = 'badge bg-primary mr-1';
     } else if (this.value === 0 || this.value === '0' || this.value === false || this.value === 'false' || this.value === 'FALSE' || this.value === 'False' || this.value === null) {
       this.label = 'No';
-      this.labelClass = 'badge badge-danger mr-1';
+      this.labelClass = 'badge bg-danger mr-1';
     } else {
       this.label = 'Not set';
-      this.labelClass = 'badge badge-default mr-1';
+      this.labelClass = 'badge bg-light text-dark';
     }
     if (this.value !== null){
       this.renderValue = this.value.toString().toUpperCase();
