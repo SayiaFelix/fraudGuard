af56511ed43dedd3c293c913cdb44303d7adb8fe
fsayia <sayia.felix@eclectics.io>
2023-07-04 15:17:08 +0300

    adding dependencies

--- src/app/views/layout/sidebar/menu.ts
+++ src/app/views/layout/sidebar/menu.ts
@@ -43,7 +43,7 @@ export const MENU: MenuItem[] = [
   },
   {
     // profiles: ['CORPORATE_ADMIN'],
-    label: 'ALL ACCREDITATION',
+    label: 'ACCREDITATION',
     icon: 'award',
     subItems: [
       {
