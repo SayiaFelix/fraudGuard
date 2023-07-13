d424d4780989b5b61e4b414725f298a402c4acf4
fsayia <sayia.felix@eclectics.io>
2023-07-13 16:10:46 +0300

    fixing bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   channelManager: 'https://test-api.ekenya.co.ke/channel/',
   mobileBanking: 'https://test-api.ekenya.co.ke/mobile-banking/',
   // customerPortalNest: 'http://localhost:8000/',
-  customerPortalNest: 'http://10.20.2.19:7782/',
+  // customerPortalNest: 'http://10.20.2.19:7782/',
+  customerPortalNest: 'https://test-api.ekenya.co.ke/tra-customer-backend/',
 
   
   // mobileBanking: 'http://10.20.2.148:7140/',
