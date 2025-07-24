13c1c1195014aa63385cb20666e672ba96dadf5e
Tecla Kyalo <kyalo.tecla@eclectics.io>
2025-07-24 08:26:52 +0300

    Changes to general page and actions page

--- src/app/views/pages/mobile-banking/customers/add-customer/add-customer.component.ts
+++ src/app/views/pages/mobile-banking/customers/add-customer/add-customer.component.ts
@@ -12,7 +12,6 @@ import Swal from "sweetalert2";
     styleUrls: ['./add-customer.component.scss']
 })
 export class AddCustomerComponent implements OnInit {
-
     @Input() title: any;
     @Input() formData: any;
     public loading = false;
@@ -29,6 +28,8 @@ export class AddCustomerComponent implements OnInit {
     chatbotdata: any;
     result: any;
     agentList: any[] = [];
+    chatbotList: any[] = [];
+    isLoadingBots = false;
 
     constructor(
         public activeModal: NgbActiveModal,
@@ -49,6 +50,7 @@ export class AddCustomerComponent implements OnInit {
         // defaultLanguage: ['', Validators.required]
     });
 
+    this.loadChatbotList();
     }
 
     onFileSelected(){ }
@@ -181,5 +183,21 @@ removeImage(){}
     if (event.target.files && event.target.files.length) {
       this.imageFile = event.target.files[0];
     }
+  }
+
+  loadChatbotList() {
+    this.isLoadingBots = true;
+    this._httpService
+      .mobileBankingPost('api/v1/corporate/admin/list-chatbots', { page: 0, size: 50 })
+      .subscribe({
+        next: (res: any) => {
+          this.chatbotList = res.data || [];
+          this.isLoadingBots = false;
+        },
+        error: () => {
+          this.chatbotList = [];
+          this.isLoadingBots = false;
+        }
+      });
   }
 }
