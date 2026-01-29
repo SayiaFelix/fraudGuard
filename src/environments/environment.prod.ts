0bce6701dcfd937f045743e8a5f4eaa32c94f65c
Felix Lucas <sayia.felix@eclectics.io>
2026-01-29 12:48:19 +0300

    setting up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
+  customerPortalNest: 'CUSTOMER_API_URL',
   standardsApi: 'STANDARDS_API_URL',
-  apiBase: 'http://130.61.111.65:3000',
-  customerPortalNest: 'http://130.61.111.65:5040/api/',
+  apiBase: 'http://130.61.111.65:3000'
   //customerPortalNest: 'https://test-api.ekenya.co.ke/tra-customer-backend/',
   //standardsApi: 'https://test-api.ekenya.co.ke/tra-backend/api/v1/',
 };
