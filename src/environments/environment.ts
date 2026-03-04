7b2920d2079134f980c93002c92b176698e46ca0
Felix Lucas <sayia.felix@eclectics.io>
2026-03-04 10:16:00 +0300

    setting the environment  api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,6 +12,6 @@
 export const environment = {
   production: false,
   apiBase: 'http://localhost:3000',
-  customerPortalNest: 'http://130.61.111.65:5040/api/',
+  customerPortalNest: 'http://127.0.0.1:5001/v1/api/',
   standardsApi: 'https://test-api.ekenya.co.ke/tra-backend/api/v1/',
 };
