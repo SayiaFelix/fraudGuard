4fdee0b07d00da848130125f081b1576d5bf2f8c
Felix Lucas <sayia.felix@eclectics.io>
2026-03-31 15:24:02 +0300

    fixing warning in the code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   standardsApi: 'STANDARDS_API_URL',
   apiBase: 'http://130.61.111.65:3000',
+  
   customerPortalNest: 'http://130.61.111.65:5001/v1/api',
-  //customerPortalNest: 'https://test-api.ekenya.co.ke/tra-customer-backend/',
-  //standardsApi: 'https://test-api.ekenya.co.ke/tra-backend/api/v1/',
+  // customerPortalNest: 'https://afa7-197-156-78-74.ngrok-free.app/v1/api',
 };
 
