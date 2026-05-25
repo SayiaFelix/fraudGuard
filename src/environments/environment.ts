bfa25c922eb36998b8638ae3d1725a8bea28af74
Felix Lucas <sayia.felix@eclectics.io>
2026-05-25 09:51:01 +0300

    redeploying the app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,8 @@
-// export const environment = {
-//   production: false,
-//   // channelManager: 'https://test-api.ekenya.co.ke/channel/',
-//   // mobileBanking: 'https://test-api.ekenya.co.ke/mobile-banking/',
-//   //customerPortalNest: 'http://localhost:8000/',
-//   // customerPortalNest: 'http://10.20.2.19:7782/',
-//   customerPortalNest: 'http://130.61.111.65:5040/api/',
-//   standardsApi: 'https://test-api.ekenya.co.ke/tra-backend/api/v1/',
-//   // standardsComment: 'http://10.20.2.19:7604/api/v1/standard/',
-// };
 
 export const environment = {
   production: false,
   apiBase: 'http://localhost:3000',
-  // customerPortalNest: 'http://127.0.0.1:5001/v1/api',
-  customerPortalNest: 'http://130.61.111.65:5001/v1/api',
+  customerPortalNest: 'http://127.0.0.1:5001/v1/api',
+  // customerPortalNest: 'http://130.61.111.65:5001/v1/api',
   standardsApi: 'https://test-api.ekenya.co.ke/tra-backend/api/v1/',
 };
